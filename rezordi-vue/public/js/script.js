b8ca916631ec4a9f069732edf352826d28843576
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-03-20 00:17:51 +0400

    Commencement formulaire réservation

--- rezordi-vue/public/js/script.js
+++ rezordi-vue/public/js/script.js
@@ -11,10 +11,10 @@ const TableaudeBord = {
                 </svg>
             </button>
 
-            <button class="btn border border-3 border-dark rounded m-auto"> Faire une réservation <br />
-            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-                <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-            </svg>
+            <button v-on:click="ajout_reservation" class="btn border border-3 border-dark rounded m-auto"> Faire une réservation <br />
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                </svg>
             </button>
 
         </div>
@@ -69,7 +69,36 @@ const TableaudeBord = {
                  <button type="button" v-on:click="envoi_utilisateur" class="btn btn-primary">Ajouter</button>
                 </span>
 
+            </form>
+        </div>
+
+
+        <div v-if="form_reservation" class="d-flex justify-content-center align-items-center"> 
+            <form class="w-50">  
+                <h2>Ajouter une réservation </h2>
+                <div class="mb-3 form-floating">
+                    <input v-model="form_date" type="date" class="form-control" name="date" id="date" placeholder="Date" required>
+                    <label class="form-label" for="date">Date</label>
+                </div>
                 
+                <div class="mb-3">
+                    <div class="row row-cols-2">
+                    
+                        <div class="col px-2">
+                            <label class="form-label" for="heure_debut">Début</label>
+                            <select v-model="form_heure_debut" class="form-control" name="heure_debut" id="heure_debut">
+                                    <option v-for="i in heures"> {{i.heure}}h </option>
+                            </select>
+                        </div>
+
+                        <div class="col px-2">
+                            <label class="form-label" for="heure_fin">Fin</label>
+                            <select v-model="form_heure_fin" class="form-control" name="heure_fin" id="heure_fin">
+                                    <option v-for="i in heures"> {{i.heure}}h </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
             </form>
         </div>
@@ -249,7 +278,12 @@ const TableaudeBord = {
             form_nom: '',
             form_prenom: '',
             form_email: '',
-            form_telephone: null
+            form_telephone: null,
+            form_date:'',
+            form_heure_debut:'',
+            form_heure_fin:'',
+            form_utilisateur_reservation:'',
+            form_poste_reservation:''
             // Details : Array pour mettre les id qu'on souhaite voir en détails (utilisateurs)
             //Affichage : Le nbre d'utilisateurs qu'on souhaite afficher
             //searchUser: L'utilisateur qu'on souhaite rechercher
@@ -266,6 +300,16 @@ const TableaudeBord = {
         },
         postes_func() {
             return this.postes;
+        },
+        heures() {
+            let heures = [];
+            for(i=0;i<24;i++){
+                if (i < 10) {
+                    heures.push({id:i, heure:'0'+i});
+                }
+                else {heures.push({id:i, heure:i}); }
+            }
+            return heures;
         }
     },
     methods: {
@@ -296,6 +340,14 @@ const TableaudeBord = {
             }
             else {
                 this.form_utilisateur = true;
+            }
+        },
+        ajout_reservation() {
+            if (this.form_reservation) {
+                this.form_reservation = false;
+            }
+            else {
+                this.form_reservation = true;
             }
         },
         envoi_utilisateur() {
